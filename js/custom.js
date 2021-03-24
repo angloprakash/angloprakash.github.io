ef3e64dc49fd5a2d8697a1d258bbfd0aade4f0da
navaar <81325647+angloprakash@users.noreply.github.com>
2021-03-25 00:41:17 +0530

    ch

--- js/custom.js
+++ js/custom.js
@@ -95,7 +95,26 @@ $(document).ready(function () {
         csubmitForm();
       }
     });
-
+    var form = document.getElementById("#contactForm");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
 
   function cformSuccess() {
     $("#contactForm")[0].reset();
